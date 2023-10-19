7546ce3c4064aab6b3e24edeee3b1764b27345fc
s-m-john <smc_john@hotmail.com>
2023-10-19 12:58:18 -0400

    STEP  5 Completed

--- ios101-lab5-flix1/ViewController.swift
+++ ios101-lab5-flix1/ViewController.swift
@@ -19,21 +19,37 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // Create the cell
-        let cell = UITableViewCell()
-        
-        // Get the movie-associated table view row
-        let movie = movies[indexPath.row]
+        // Create, configure, and return a table view cell for the given row (i.e., `indexPath.row`)
 
+            print("🍏 cellForRowAt called for row: \(indexPath.row)")
 
-        // Configure the cell (i.e. update UI elements like labels, image views, etc.)
-        // Get the row where the cell will be placed using the `row` property on the passed in `indexPath` (i.e., `indexPath.row`)
-        cell.textLabel?.text = movie.title
-        
-        print("🍏 cellForRowAt called for row: \(indexPath.row)")
+            // Get a reusable cell
+            // Returns a reusable table-view cell object for the specified reuse identifier and adds it to the table. This helps optimize table view performance as the app only needs to create enough cells to fill the screen and reuse cells that scroll off the screen instead of creating new ones.
+            // The identifier references the identifier you set for the cell previously in the storyboard.
+            // The `dequeueReusableCell` method returns a regular `UITableViewCell`, so we must cast it as our custom cell (i.e., `as! MovieCell`) to access the custom properties you added to the cell.
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
 
-        // Return the cell for use in the respective table view row
-        return cell
+            // Get the movie associated table view row
+            let movie = movies[indexPath.row]
+
+            // Configure the cell (i.e., update UI elements like labels, image views, etc.)
+
+            // Unwrap the optional poster path
+            if let posterPath = movie.poster_path,
+
+                // Create a url by appending the poster path to the base url. https://developers.themoviedb.org/3/getting-started/images
+               let imageUrl = URL(string: "https://image.tmdb.org/t/p/w500" + posterPath) {
+
+                // Use the Nuke library's load image function to (async) fetch and load the image from the image URL.
+                Nuke.loadImage(with: imageUrl, into: cell.posterImageView)
+            }
+
+            // Set the text on the labels
+            cell.titleLabel.text = movie.title
+            cell.overviewLabel.text = movie.overview
+
+            // Return the cell for use in the respective table view row
+            return cell
     }
     
 
